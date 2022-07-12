8d532ee27b3f30ccb68e082806243964b8c06077
救赎呐 <yueqing827@163.com>
2022-07-12 15:26:57 +0800

    创建commons模块信息

--- crazy-data-commons/src/main/java/cn/ekgc/crazy/base/pojo/entity/BaseEntity.java
+++ crazy-data-commons/src/main/java/cn/ekgc/crazy/base/pojo/entity/BaseEntity.java
@@ -1,4 +1,20 @@
 package cn.ekgc.crazy.base.pojo.entity;
 
-public class BaseEntity {
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class BaseEntity implements Serializable {
+	private String staus;					//系统状态：Y-启用  N-禁用
+	@TableField("createdBy")
+	private String createdBy;                   // 创建人
+	@TableField("createTime")
+	private Date createTime;                    // 创建时间
+	@TableField("modifiedBy")
+	private String modifiedBy;                  // 修改人
+	@TableField("modifiedTime")
+	private Date modifiedTime;                  // 修改时间
 }
--- crazy-patient-provider/src/main/java/cn/ekgc/crazy/CrazyPatientProviderStarter.java
+++ crazy-patient-provider/src/main/java/cn/ekgc/crazy/CrazyPatientProviderStarter.java
@@ -1,11 +1,13 @@
 package cn.ekgc.crazy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("cn.ekgc.crazy.dao")
 public class CrazyPatientProviderStarter {
     public static void main(String[] args) {
         SpringApplication.run(CrazyPatientProviderStarter.class, args);
