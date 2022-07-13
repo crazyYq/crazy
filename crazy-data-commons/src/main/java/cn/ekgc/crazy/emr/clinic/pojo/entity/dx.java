662bc3f6f710676e97255b597582d2dabcfe9466
ASUS <l1537683893@163.com>
2022-07-13 11:28:54 +0800

    添加clinlc实体

---
+++ crazy-data-commons/src/main/java/cn/ekgc/crazy/emr/clinic/pojo/entity/dx.java
@@ -0,0 +1,24 @@
+package cn.ekgc.crazy.emr.clinic.pojo.entity;
+
+import cn.ekgc.crazy.base.pojo.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+@TableName("emr_clinic_dx")
+public class dx extends BaseEntity {
+	private Long id;                              //主键
+	private String emrClinic;                     //所属门（急）诊病历
+	private String dxOrgName;                     //诊断机构名称
+	private Date dxDate;                          //诊断日期
+	private String dxCat;                         //诊断类别
+	private String dxCatCode;                     //诊断类别代码
+	private String dxCisCode;                     //诊断顺位（从属关系）代码
+	private String disease;                       //疾病名称
+	private String diseaseCode;                   //疾病代码
+	private String dxAccording;                   //诊断依据
+	private String dxAccordingCode;               //诊断依据代码
+
+}
