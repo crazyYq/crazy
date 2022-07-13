bb8204db3dd622298708b3e9930aefb91f63dc8c
ASUS <maguasen@163..com>
2022-07-13 12:56:24 +0800

    emr下的实体类补充

---
+++ crazy-data-commons/src/main/java/cn/ekgc/crazy/emr/referral/pojo/entity/Referral.java
@@ -0,0 +1,16 @@
+package cn.ekgc.crazy.emr.referral.pojo.entity;
+
+import cn.ekgc.crazy.base.pojo.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * <b></b>
+ *
+ * @author ASUS
+ * @data 2022/7/13
+ */
+@Data
+@TableName("emr_referral_record")
+public class Referral extends BaseEntity {
+}
